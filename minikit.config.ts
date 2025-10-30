2fd052e409fe6e84882f295d2fa4fb72cf2cc7ba
GoJackzi <captgobunath2@gmail.com>
2025-10-31 03:24:17 +0800

    Add account association credentials

--- minikit.config.ts
+++ minikit.config.ts
@@ -3,10 +3,9 @@ const ROOT_URL = process.env.NEXT_PUBLIC_APP_URL ||
 
 export const minikitConfig = {
   accountAssociation: {
-    // This will be filled in after account association via Base Build
-    "header": "",
-    "payload": "",
-    "signature": ""
+    "header": "eyJmaWQiOjI2NjM3MSwidHlwZSI6ImF1dGgiLCJrZXkiOiIweDdGMjUxZjFBOTREMzE4MTQxREZFQkNlM0E3MURFN0Q1MDU4ODg5MkMifQ",
+    "payload": "eyJkb21haW4iOiJuZXctbWluaS1hcHAtcXVpY2tzdGFydC1saWFyZC52ZXJjZWwuYXBwIn0",
+    "signature": "WNK+RqGoDnVI+ADGZmgEt2fN/jLGj1i4E8Y9KtevrIsVMlBT7vZzMUk9l5ldFrnVL5y+1pikWfpCJJwABDjQxxw="
   },
   miniapp: {
     version: "1",
